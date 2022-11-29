fdf73bafd18c7800668929f8e3d4a0a050bfc29f
Nitin Namdev <nitinnamdeo456@gmail.com>
2022-11-29 17:21:53 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -24,7 +24,7 @@ for role in $roles; do
   for dir in $dirs; do
     mkdir -p "roles/$role/$dir"
     if [[ "$dir" == 'templates' ]]; then touch "roles/$role/$dir/server.conf.j2" && continue; fi
-    if [[ "$dir" == 'library' ]]; then git clone https://gist.github.com/Vortexdude/09292a3cd3690b0ee27828108d05b842 "roles/$role/$dir" && continue; fi
+    if [[ "$dir" == 'library' ]]; then git clone https://gist.github.com/Vortexdude/09292a3cd3690b0ee27828108d05b842 "roles/$role/$dir" && rm -rf roles/$role/$dir/.git && continue; fi
     touch "roles/$role/$dir/main.yml"
   done
   echo "Your role is $role is created Succesfully "
