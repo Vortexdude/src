541b20aba524f6fa53a18dbe181f47fbe784c362
Nitin Namdev <nitinnamdeo456@gmail.com>
2022-11-30 10:51:05 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+
+#all the roles are here
 roles="${@:-test_role}"
 
 
 #creating the ansible directory, playbook, ansible.cfg, and inventory
 mkdir -p ansible{/inventory,/roles}  && cd ansible
 
+#creating playbook, configuration and inventory files
 files="main.yml ansible.cfg inventory/all"
 for file in $files; do touch $file; done
 
@@ -15,6 +18,7 @@ inventory = inventory/all
 
 EOF
 
+#Getting all the roles
 for role in $roles; do
 
 # list of directories
@@ -23,8 +27,14 @@ for role in $roles; do
 #craeting the directories and files
   for dir in $dirs; do
     mkdir -p "roles/$role/$dir"
+    
+    #creating template file
     if [[ "$dir" == 'templates' ]]; then touch "roles/$role/$dir/server.conf.j2" && continue; fi
+    
+    #creating module file
     if [[ "$dir" == 'library' ]]; then wget https://raw.githubusercontent.com/Vortexdude/src/main/hello.py -O roles/$role/$dir/hello.py && continue; fi
+    
+    #creating taks file
     if [[ "$dir" = 'tasks' ]]; then
 cat <<EOF  >> "roles/$role/$dir/main.yml"
 - name: New hello module
@@ -40,17 +50,20 @@ EOF
 continue
 fi
     
+    #create rest of the file
     touch "roles/$role/$dir/main.yml"
   done
   echo "Your role is $role is created Succesfully "
 done
+
+#Creating playbook
 cat <<EOF  >> main.yml
 - hosts: localhost
   become: true
   roles:
   
 EOF
-
+# include role in the playbook
 for role in $roles; do
 cat << EOF >>main.yml
     - { role: $role }
