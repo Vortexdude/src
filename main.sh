24a06838e7d810646a40773d70f1c255c102e4cf
Nitin Namdev <nitinnamdeo456@gmail.com>
2022-12-08 15:46:10 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -15,11 +15,12 @@ if [[ "${#}" -lt 1 ]]; then usage && exit 1; fi
 os_version=$(cat /etc/os-release | grep PRETTY_NAME | awk -F= '{print $2}' | tr -d '"' | awk '{print $1}')
 
 # installing ansible 
+echo "**** Installing ansible"
 if [[ "${os_version}" -eq "Ubuntu" ]]; then apt install ansible jq -y 2>error.log >/dev/null; else yum install ansible jq -y 2>error.log >/dev/null; fi
 
 umask 77 && mkdir -p ${clone_path}
-git clone -b ${branch_name} ${clone_url} "${clone_path}"
-
+echo "**** Cloning the repo ${clone_url} in the branch ${branch_name}"
+git clone -b ${branch_name} ${clone_url} "${clone_path}" &>/dev/null
 # Creating var file
 echo "users: " >${clone_path}/vars.yml
 for name in "${@}"
@@ -32,8 +33,8 @@ done
 # run the ansible playbook
 server=localhost
 connection=local
-echo (ls -l )
-echo (pwd)
+echo "**** Running Ansible playbook"
 echo $(ansible-playbook ${clone_path}/local.yml -i ${server}, -c ${connection} -e "@${clone_path}/vars.yml")
-#rm -rf ${clone_path}
+echo "**** Deleting temprary files"
+rm -rf ${clone_path}
 if [[ "${?}" -eq 0 ]]; then echo "**** Succesfully created ${#} users - ${@}" ; else "****  There might be an issue" && exit 1; fi
