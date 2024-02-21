0e34c2d4c3584f711e53193131b0f69e0322cb46
Nitin Namdev <nitinnamdeo456@gmail.com>
2024-02-21 15:35:58 +0530

    Update install_java.sh

--- install_java.sh
+++ install_java.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 path=/opt/java/
+cd $path
 
-wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz
-mv jdk-8u131-linux-x64.tar.gz $path
+wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.tar.gz
+
+
+tar -xvf jdk-17_linux-x64_bin.tar.gz
+cd jdk-17.0.10
+
+export PATH=$PATH:/${path}jdk-17.0.10/bin
